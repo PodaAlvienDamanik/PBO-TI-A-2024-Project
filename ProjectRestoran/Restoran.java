b022d52d95ba41d392d5f0859f2a409442270d65
Alvin Damanik <2381061@unai.edu>
2024-10-07 22:54:28 +0700

    editan Poda

--- ProjectRestoran/Restoran.java
+++ ProjectRestoran/Restoran.java
@@ -7,17 +7,27 @@ public class Restoran {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         while (true) {
-            System.out.println("Selamat Datang di Restoran Asian FI");
+            System.out.println("Selamat Datang di Restoran Mandalika");
             System.out.println("Silakan Masukkan Pilihan Anda");
             System.out.println("1.Penjual");
             System.out.println("2.Pembeli");
             System.out.println("3.Keluar dari Aplikasi");
             System.out.print("Pilihan Anda: ");
             int pilihan = input.nextInt();
+            input.nextLine();
 
             switch (pilihan){
                 case 1:
-                    crudPenjual.run(input);
+                    System.out.print("Masukkan Username Anda: ");
+                    String inputUsername = input.nextLine();
+                    System.out.print("Masukkan Password Anda: ");
+                    int inputPassword = input.nextInt();
+                    if (cekUserNamePenjual(inputUsername, inputPassword)) {
+                        crudPenjual.run(input);
+                    }else{
+                        System.out.println("Username atau password anda tidak valid!");
+
+                    }
                     break;
 
                 case 2:
@@ -34,6 +44,13 @@ public class Restoran {
                     break;
 
             }
+
         }
+    }
+    private static boolean cekUserNamePenjual(String username,int password){//07 Oktober 2024/22.53 WIB Malam Membuat fitur password untuk penjual
+        if (password == 1234){
+            return username.equals("Poda") || username.equals("Arvind") || username.equals("Yesaya");
+        }
+        return false;
     }
 }
