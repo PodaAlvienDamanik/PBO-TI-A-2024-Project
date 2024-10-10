c79faa982bf26c87ceb512193c765704a4c31d04
Yesaya Sitompul <2381050@unai.edu>
2024-10-10 09:22:55 +0700

    Editan yesaya hari ini 10/10/2024

--- ProjectRestoran/crudPembeli.java
+++ ProjectRestoran/crudPembeli.java
@@ -115,6 +115,9 @@ public class crudPembeli {
                     break;
 
                 case 4:
+                    System.out.print("Masukkan nomor pesanan yang ingin dihapus: "); //dibuat tanggal 10/10/2024 jam 09:22
+                    int nomorHapus = scanner.nextInt() - 1;
+                    hapusPesanan(nomorHapus);
                     break;
 
                 case 5:
